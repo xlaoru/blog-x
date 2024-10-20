4b80b165527c78658b71305cfcd95252a8a7bbd5
xlaoru <sia212007@gmail.com>
2024-10-20 18:21:28 +0300

    v24.1

--- src/components/CommentItem.tsx
+++ src/components/CommentItem.tsx
@@ -1,12 +1,12 @@
-import React from "react";
 import { IComment } from "../store/BlogSlice";
 
 type ICommentItemProps = Omit<IComment, "_id"> & { userName: string };
 
 export default function CommentItem({ text, createdBy, userName }: ICommentItemProps) {
     return (
-        <div>
-            {text}. | Created by: <u>{isNaN(Number(createdBy)) ? createdBy : userName}</u>
+        <div className="Comment">
+            <div className="text">{text}</div>
+            <div className="created-by">Created by: <b>{isNaN(Number(createdBy)) ? createdBy : userName}</b></div>
         </div>
     )
 }
