439b9f90945671573849e161939b34a82295583e
xlaoru <sia212007@gmail.com>
2024-11-28 22:29:35 +0200

    v1.3

--- src/components/RichTextEditor.tsx
+++ src/components/RichTextEditor.tsx
@@ -78,7 +78,7 @@ export default function RichTextEditor({ content, setContent }: IRichTextEditorP
             onChange={handleChange}
             modules={modules}
             theme="snow"
-            placeholder="Начните писать здесь..."
+            placeholder="Write something amazing..."
         />
     );
 }
